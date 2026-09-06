f0f5c6034b57f12d0fc9b5966bdc7a3d92a429be
isaacrbell2017-cloud <isaacrbell2017@gmail.com>
2026-09-06 20:35:51 +0100

    Update main.pp

--- src/main.pp
+++ src/main.pp
@@ -2,10 +2,9 @@
 #include <Geode/modify/EditorUI.hpp>
 
 using namespace geode::prelude;
-
-class $modify(GDDecoEditorUI, EditorUI) {
+ $modify(GDDecoEditorUI, EditorUI) {
     bool init(LevelEditorLayer* editorLayer) {
-        if (!EditorUI::init(editorLayer))
+         (!EditorUI::init(editorLayer))
             return false;
 
         log::info("GD Deco AI: Editor opened!");
