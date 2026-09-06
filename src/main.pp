dc67c8cead7506ef550c096ce49de6c3a3a5ca49
isaacrbell2017-cloud <isaacrbell2017@gmail.com>
2026-09-06 17:48:38 +0100

    Update main.pp

--- src/main.pp
+++ src/main.pp
@@ -1,1 +1,15 @@
-// GD Deco AI v0.4 source
+#include <Geode/Geode.hpp>
+#include <Geode/modify/EditorUI.hpp>
+
+using namespace geode::prelude;
+
+class $modify(GDDecoEditorUI, EditorUI) {
+    bool init(LevelEditorLayer* editorLayer) {
+        if (!EditorUI::init(editorLayer))
+            return false;
+
+        log::info("GD Deco AI: Editor opened!");
+
+        return true;
+    }
+};
